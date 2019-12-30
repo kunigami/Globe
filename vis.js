de4ce8e4ba1cd302c7f6417f90611b9aa0aaf932
Guilherme Kunigami <kunigami@gmail.com>
2019-12-29 21:07:36 -0800

    format, fix stop mechanism

--- vis.js
+++ vis.js
@@ -1,15 +1,18 @@
 var feature; // eventually: all svg paths (countries) of the world
 //  , toggle; // animation on/off control
 
+// Keeps track of animation
+let timer;
+
 var projection = d3.geoOrthographic()
     .scale(380)
-    .rotate([71.03,-42.37])
+    .rotate([71.03, -42.37])
     .clipAngle(90)
-//    .rotate()
-    
-//    .mode("orthographic")
+    //    .rotate()
+
+    //    .mode("orthographic")
     .translate([400, 400]);
-    
+
 //var rotate = [-71.03, 42.37],
 //    velocity = [.018, .006];
 
@@ -29,47 +32,48 @@ var path = d3.geoPath()
     .projection(projection);
 
 var svg = d3.select("#body").append("svg:svg")
-    .attr("width",  "100%")
+    .attr("width", "100%")
     .attr("height", "100%")
     .on("mousedown", mousedown);
 
 if (frameElement) frameElement.style.height = '800px';
 //if (frameElement) frameElement.style.width = '1500px';
 
-d3.json("https://gist.githubusercontent.com/phil-pedruco/10447085/raw/426fb47f0a6793776a044f17e66d17cbbf8061ad/countries.geo.json", function(collection) {
-  feature = svg.selectAll("path")
-      .data(collection.features)
-    .enter().append("svg:path")
-      .attr("d",clip);
+d3.json("https://gist.githubusercontent.com/phil-pedruco/10447085/raw/426fb47f0a6793776a044f17e66d17cbbf8061ad/countries.geo.json", function (collection) {
+    feature = svg.selectAll("path")
+        .data(collection.features)
+        .enter().append("svg:path")
+        .attr("d", clip);
 
-  feature.append("svg:title")
-      .text(function(d) { return d.properties.name; });
+    feature.append("svg:title")
+        .text(function (d) { return d.properties.name; });
 
-  stopAnimation();
-  d3.select('#animate').on('click', function () {
-    if (done) startAnimation(); else stopAnimation();
-  });
+    stopAnimation();
+    d3.select('#animate').on('click', function () {
+        if (done) startAnimation(); else stopAnimation();
+    });
 });
 
 function stopAnimation() {
-  done = true;
-  d3.select('#animate').node().checked = false;
+    done = true;
+    d3.select('#animate').node().checked = false;
+    timer && timer.stop();
 }
 
 function startAnimation() {
-  done = false;
-  d3.timer(function() {
-    var rotate = projection.rotate();
-    rotate = [rotate[0] + 0.1, rotate[1]];
-    projection.rotate(rotate);
-//    circle.center(rotate);
-    refresh();
-    return done;
-  });
+    done = false;
+    timer = d3.timer(function () {
+        var rotate = projection.rotate();
+        rotate = [rotate[0] + 0.1, rotate[1]];
+        projection.rotate(rotate);
+        //    circle.center(rotate);
+        refresh();
+        return done;
+    });
 }
 
 function animationState() {
-  return 'animation: '+ (done ? 'off' : 'on');
+    return 'animation: ' + (done ? 'off' : 'on');
 }
 
 d3.select(window)
@@ -84,43 +88,38 @@ d3.select(window)
 //});
 
 var m0
-  , o0
-  , done
-  ;
+    , o0
+    , done
+    ;
 
 function mousedown() {
-  stopAnimation();
-  m0 = [d3.event.pageX, d3.event.pageY];
-  o0 = projection.rotate();
-  d3.event.preventDefault();
+    stopAnimation();
+    m0 = [d3.event.pageX, d3.event.pageY];
+    o0 = projection.rotate();
+    d3.event.preventDefault();
 }
 
 function mousemove() {
-  if (m0) {
-    var m1 = [d3.event.pageX, d3.event.pageY]
-      , o1 = [o0[0] - (m0[0] - m1[0]) / 8, o0[1] - (m1[1] - m0[1]) / 8];
-    projection.rotate(o1);
-//    circle.center(o1);
-    refresh();
-  }
+    if (m0) {
+        var m1 = [d3.event.pageX, d3.event.pageY]
+            , o1 = [o0[0] - (m0[0] - m1[0]) / 8, o0[1] - (m1[1] - m0[1]) / 8];
+        projection.rotate(o1);
+        //    circle.center(o1);
+        refresh();
+    }
 }
 
 function mouseup() {
-  if (m0) {
-    mousemove();
-    m0 = null;
-  }
+    if (m0) {
+        mousemove();
+        m0 = null;
+    }
 }
 
 function refresh(duration) {
-  (duration ? feature.transition().duration(duration) : feature).attr("d",clip);
+    (duration ? feature.transition().duration(duration) : feature).attr("d", clip);
 }
 
 function clip(d) {
-  return path(d);
-}
-
-function reframe(css) {
-  for (var name in css)
-    frameElement.style[name] = css[name] + 'px';
+    return path(d);
 }
